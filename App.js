1c0f9a215f5a6de6e3dc87910fb21944c4c59ec0
rohanghosh <rohangho@live.com>
2023-01-28 23:14:38 +0530

    added sepeate class file foe smaller components

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello World this is a demo apoop</Text>
       <StatusBar style="auto" />
     </View>
   );
